a98de66c7d775af14a13601168d8cc5792097403
Vipin Arya <sachin.arya@preplaced.in>
2023-09-30 14:52:55 +0530

    dev build using parcel

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Exercises/Code practice while learning react in namaste-react course.
 
 >> NPM does not stands for Node Package Manager
 >> BUNDELERS : Parcel, Webpack, Vite, Rollup etc
->> 
+
+
+>> ~ : used for automatically installing latest patch
+>> ^ : used for automatically installing latest minor version
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { ReactDOM } from "react-dom/client";
-const heading = React.createElement("h1", { key: 1 }, "Hello React H1!");
+// const heading = React.createElement("h1", { key: 1 }, "Hello React H1!");
 
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading-two", key: 2 },
-  "Hello React H2!"
-);
-const container = React.createElement("div", { id: "container" }, [
-  heading,
-  heading2,
-]);
+// const heading2 = React.createElement(
+//   "h2",
+//   { id: "heading-two", key: 2 },
+//   "Hello React H2!"
+// );
+// const container = React.createElement("div", { id: "container" }, [
+//   heading,
+//   heading2,
+// ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(container);
-root.render(container);
+// console.log(container);
+// root.render(container);
+console.log("Hey");
