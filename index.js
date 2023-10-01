e2fd5980bb1a0e65aca58bdf94a0d02d94bdce66
Sachin Arya <sachin.arya@preplaced.in>
2023-10-01 11:03:31 +0530

    JSX , Component , Component composition

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 
 //JSX
-const  div = (<div><h1>Heading</h1><h1>Heading2</h1></div>)
+const  span = (<span>I am span</span>)
+
+// Component 
+const Fun = ()=>{
+    return <div><h1>Heading</h1><h1>Heading2</h1></div>
+}
+
+//Component composition
+function Main(){
+    return <div>
+        {span}
+        <Fun/>
+        <i>I am another react element.</i>
+    </div>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(div);
+root.render(<Main/>);
