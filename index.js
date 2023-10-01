da98f223483ff6b53f6e9b8c72798eb49565d588
Sachin Arya <sachin.arya@preplaced.in>
2023-10-01 15:02:12 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import  {ReactDOM}  from "react-dom/client";
 
+
 const header = React.createElement("div", {}, "Heading")
 const root = ReactDOM.createRoot("div", {}, document.getElementById("root"));
 root.render(header)
