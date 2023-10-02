8cb5a3d5a525dfc5009c935c6dd8952f87fcd1f6
Sachin Arya <sachin.arya@preplaced.in>
2023-10-03 00:00:06 +0530

    logic more clear

--- src/RestaurantList.jsx
+++ src/RestaurantList.jsx
@@ -6,28 +6,31 @@ import * as Restaurant from "./RestaurantCard";
 
 function RestauRantList() {
     const [searchText, setSearchText] = useState("");
-    const [filteredData, setFilteredData] = useState(dataObject)
+    const [configData, setConfigData] = useState(dataObject)
     function handleSearchTextValue(e) {
         setSearchText(e.target.value);
     }
 
     function filterCard(text, data) {
         const filteredData = data.filter((item) => item.info.name.includes(text));
-        setFilteredData(filteredData);
+        setConfigData(filteredData);
     }
 
     return (
         <>
             <div className="search-bar">
-                <input type="text" placeholder="Search..." value={searchText} onChange={(event) => handleSearchTextValue(event)} onInput={() => filterCard(searchText, dataObject)} />
-                <button onClick={() => filterCard(searchText, dataObject)} className="button">Search</button>
+                <input type="text" placeholder="Search..." value={searchText}
+                    onChange={(event) => handleSearchTextValue(event)}
+                    onInput={() => setConfigData(dataObject)}
+                />
+                <button onClick={() => filterCard(searchText, configData)} className="button">Search</button>
             </div >
 
             <div className="restaurant-list">
-                {filteredData.map((item, index) => (
-                    <div className="restaurant-card" key={filteredData[index].info.name}>
-                        <Restaurant.RestaurantCard {...filteredData[index].info} />
-                        {parseInt(filteredData[index].info.rating.votes) > 1 && <Restaurant.Recommended />}
+                {configData.map((item, index) => (
+                    <div className="restaurant-card" key={configData[index].info.name}>
+                        <Restaurant.RestaurantCard {...configData[index].info} />
+                        {parseInt(configData[index].info.rating.votes) > 1 && <Restaurant.Recommended />}
                     </div>
                 ))}
             </div>
