ff4258c64e61246f6298c06c3fc45b02267f7bd2
Sachin Arya <sachin.arya@preplaced.in>
2023-10-02 23:15:57 +0530

    normal variable changes is not tracked by react, it wont render UI for updated data

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,23 @@
 function Header() {
+    let title = "Title"
+    function fun() {
+        title = "New";
+        console.log(title);
+    }
     return (
         <div className="header">
             <a href="/" className="logo">
                 <img alt="logo" src="https://imgs.search.brave.com/F_CYamxHqVTQRWPNwX_Uy7OZCHnKI5rMvGUHLK7t940/rs:fit:500:0:0/g:ce/aHR0cHM6Ly92aWxs/YWVzY29uZGlkYXNl/YXR0bGUuY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE5LzA0/L3ZpbGxhLXNsaWRl/ci05LmpwZw" />
             </a>
+            <h2>{title}</h2>
             <ul>
                 <li>About</li>
                 <li>Service</li>
                 <li>Contact</li>
                 <li>Cart</li>
             </ul>
+
+            <button onClick={fun}>Change</button>
         </div>
     )
 }
