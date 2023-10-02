efd4c336506d9d331e6a9194a12ff91f5492ff04
Sachin Arya <sachin.arya@preplaced.in>
2023-10-02 13:26:53 +0530

    value with normal js variable given to a react element can't be changed

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,12 +1,13 @@
-function handleSearchTextValue(text) {
+function handleSearchTextValue(e, text) {
+    text = e.target.value
     console.log(text);
 }
 
 function SearchBar() {
-    const searchText = "Text"
+    let searchText = "Text"
     return (
         <div className="search-bar">
-            <input type="text" placeholder="Search..." value={searchText} />
+            <input type="text" placeholder="Search..." value={searchText} onChange={() => handleSearchTextValue(event, searchText)} />
         </div>
     )
 }
