106b24786683f64bf7e6fa04f2649f7fe5e8e7bc
Sachin Arya <sachin.arya@preplaced.in>
2023-10-04 02:00:34 +0530

    search from initil data and map over filtered data, both should be original data initilly

--- src/RestaurantCard.jsx
+++ src/RestaurantCard.jsx
@@ -1,10 +1,10 @@
 export function RestaurantCard({ name, image, cuisine, locality }) {
     return (
         <>
-            <img alt={name} src={image.url} />
+            <img alt={name} src={image?.url} />
             <h2>{name}</h2>
-            <h3>{[cuisine[0].name, cuisine[1].name].join(", ")}</h3>
-            <h4>{locality.name}</h4>
+            <h3>{[cuisine[0]?.name, cuisine[1]?.name].join(", ")}</h3>
+            <h4>{locality?.name}</h4>
         </>
     )
 }
--- src/RestaurantList.jsx
+++ src/RestaurantList.jsx
@@ -6,33 +6,31 @@ import * as Restaurant from "./RestaurantCard";
 
 function RestauRantList() {
     const [searchText, setSearchText] = useState("");
-    const [configData, setConfigData] = useState(dataObject);
-    const [filteredData, setFilteredData] = useState(dataObject);
+    const [restaurantData, setRestaurantData] = useState(dataObject);
+    const [filteredRestaurantData, setFilteredRestaurantData] = useState(dataObject);
+
+    function filterRestaurant(text, data) {
+        const filtereData = data.filter((item) => item?.info?.name?.toLowerCase().includes(text.toLowerCase()));
+        setFilteredRestaurantData(filtereData);
+    }
 
     function handleSearchTextValue(e) {
         setSearchText(e.target.value);
     }
 
-    function filterCard(text, data) {
-        const filteredData = data.filter((item) => item?.info?.name?.toLowerCase().includes(text.toLowerCase()));
-        setConfigData(filteredData);
-    }
 
     return (
         <>
             <div className="search-bar">
-                <input type="text" placeholder="Search..." value={searchText}
-                    onChange={(event) => handleSearchTextValue(event)}
-                    onInput={() => setConfigData(dataObject)}
-                />
-                <button onClick={() => filterCard(searchText, configData)} className="button">Search</button>
+                <input type="text" placeholder="Search..." value={searchText} onChange={(event) => handleSearchTextValue(event)} />
+                <button onClick={() => filterRestaurant(searchText, restaurantData)} className="button">Search</button>
             </div >
 
             <div className="restaurant-list">
-                {configData.map((item, index) => (
-                    <div className="restaurant-card" key={configData[index].info.name}>
-                        <Restaurant.RestaurantCard {...configData[index].info} />
-                        {parseInt(configData[index].info.rating.votes) > 1 && <Restaurant.Recommended />}
+                {filteredRestaurantData.map((item, index) => (
+                    <div className="restaurant-card" key={filteredRestaurantData[index]?.info?.name}>
+                        <Restaurant.RestaurantCard {...filteredRestaurantData[index]?.info} />
+                        {parseInt(filteredRestaurantData[index]?.info?.rating?.votes) > 1 && <Restaurant.Recommended />}
                     </div>
                 ))}
             </div>
