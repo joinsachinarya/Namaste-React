c012ee609999d7b09182e8ace2f6cad217cf0f6a
Sachin Arya <sachin.arya@preplaced.in>
2023-10-07 23:32:00 +0530

    changing state in functional component

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 
 function Profile({ topic }) {
-    const [day] = useState(0)
+    const [day, setDay] = useState(0)
 
     return (
         <h2>
             Functional component : Profile
             <p>Topic: {topic}</p>
             <i>Day: {day}</i>
+            <button onClick={() => setDay(1)}>Chnage day</button>
         </h2>
     )
 }
--- src/ProfileClassComponent.jsx
+++ src/ProfileClassComponent.jsx
@@ -1,19 +1,21 @@
 import { Component } from "react";
 
 class ProfileClassComponent extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     // this.state.myState = {
-    //     //     name: "Sachin",
-    //     //     age: 23
-    //     // }
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            day: 0,
+            day2: 0
+        }
+    }
     render() {
         return (
             <h2>
                 Class component : Profile
                 <p>Topic: {this.props.topic}</p>
-                <i>Day: {""}</i>
+                <i>Day: {this.state.day}</i>
+                {/* <i>{this.state.day2}</i> */}
+                <button onClick={() => this.setState({ day: 1 })}>Change day</button>
             </h2>
         )
     }
