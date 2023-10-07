9b3b49559be0f5938e7161f815baf696deb95287
Sachin Arya <sachin.arya@preplaced.in>
2023-10-07 23:06:32 +0530

    for recieving props constructor is not necessary in class component

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
+
 function Profile({ topic }) {
+    const [day] = useState(0)
 
     return (
         <h2>
             Functional component : Profile
             <p>Topic: {topic}</p>
+            <i>Day: {day}</i>
         </h2>
     )
 }
--- src/ProfileClassComponent.jsx
+++ src/ProfileClassComponent.jsx
@@ -3,16 +3,17 @@ import { Component } from "react";
 class ProfileClassComponent extends Component {
     // constructor(props) {
     //     super(props);
-    //     this.state.myState = {
-    //         name: "Sachin",
-    //         age: 23
-    //     }
+    //     // this.state.myState = {
+    //     //     name: "Sachin",
+    //     //     age: 23
+    //     // }
     // }
     render() {
         return (
             <h2>
                 Class component : Profile
                 <p>Topic: {this.props.topic}</p>
+                <i>Day: {""}</i>
             </h2>
         )
     }
