c26ede3323ab98baabe0dbbb18ce3b39db580b9b
Sachin Arya <sachin.arya@preplaced.in>
2023-10-15 18:46:12 +0530

    we can override the value of context data using Contxt.Provider

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import Header from "./src/components/Header";
 import Footer from "./src/components/Footer";
 import { Outlet } from "react-router-dom";
+import UserContext from "./src/context/UserContext";
+import { useState } from "react";
 
 function App() {
-
+    const [user, setUser] = useState({
+        name: "Sachin Arya",
+        email: "sachinkarya333@gmail.com"
+    })
     return (
-        <>
+        <UserContext.Provider value={user}>
             <Header />
             <Outlet />
             <Footer />
-        </>
+        </UserContext.Provider>
     )
 }
 
