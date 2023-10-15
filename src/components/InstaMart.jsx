ea7086d254324617b7f5cbfac97e50fdf6175998
Sachin Arya <sachin.arya@preplaced.in>
2023-10-15 16:56:07 +0530

    controling accordian's states from parent

--- src/components/InstaMart.jsx
+++ src/components/InstaMart.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import InstaMartAccordion from "./InstaMartAccordion";
 
 function InstaMart() {
+    const [visibleDescConfig, setVisibleDescConfig] = useState({
+        isTeamVisible: false,
+        isStoreVisible: false,
+        isServicesVisible: false,
+    })
+
     return (
         <div className="flex flex-col flex-wrap gap-10 p-10 mb-10">
-            <InstaMartAccordion title={"Team"} />
-            <InstaMartAccordion title={"Store"} />
-            <InstaMartAccordion title={"Services"} />
+            <InstaMartAccordion title={"Team"} isDescVisible={visibleDescConfig.isTeamVisible} setIsDescVisible={() => setVisibleDescConfig({
+                isTeamVisible: true,
+                isStoreVisible: false,
+                isServicesVisible: false,
+            })} />
+            <InstaMartAccordion title={"Store"} isDescVisible={visibleDescConfig.isStoreVisible} setIsDescVisible={() => setVisibleDescConfig({
+                isTeamVisible: false,
+                isStoreVisible: true,
+                isServicesVisible: false,
+            })} />
+            <InstaMartAccordion title={"Services"} isDescVisible={visibleDescConfig.isServicesVisible} setIsDescVisible={() => setVisibleDescConfig({
+                isTeamVisible: false,
+                isStoreVisible: false,
+                isServicesVisible: true,
+            })} />
         </div>
     )
 }
--- src/components/InstaMartAccordion.jsx
+++ src/components/InstaMartAccordion.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-function InstaMartAccordion({ title }) {
-    const [isDescVisible, setIsDesVisible] = useState(false);
+function InstaMartAccordion({ title, isDescVisible, setIsDescVisible }) {
+
+    const handleIsDescVisible = () => {
+        setIsDescVisible(!isDescVisible);
+    }
+
     return (
         <div className="border p-5">
             <p className="font-bold text-2xl">{title}</p>
-            <button onClick={() => setIsDesVisible(!isDescVisible)} className="underline font-semibold">{isDescVisible ? "Hide" : "Show"}</button>
+            <button
+                onClick={handleIsDescVisible}
+                className="underline font-semibold">{isDescVisible ? "Hide" : "Show"}</button>
             {isDescVisible &&
                 <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, porro! Totam laborum, at eveniet numquam suscipit repellat sequi voluptate sapiente quod quia nobis saepe ex recusandae excepturi tenetur aspernatur aliquid. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id saepe suscipit quibusdam magnam molestias, dignissimos porro itaque totam reprehenderit excepturi iusto sapiente, soluta tempore minima labore repellat! Excepturi, ab modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem libero fugiat tempore ex, dolor illo ipsam alias nemo beatae impedit harum corrupti iure, distinctio, qui neque provident quia laudantium ipsa!</p>
             }
