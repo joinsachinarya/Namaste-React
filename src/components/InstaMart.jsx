bcb3e8d2e5134b977ff17b36c29dfab8b7b6b397
Sachin Arya <sachin.arya@preplaced.in>
2023-10-15 15:44:25 +0530

    add accordion component in instamart

--- src/components/InstaMart.jsx
+++ src/components/InstaMart.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import InstaMartAccordion from "./InstaMartAccordion";
 
 function InstaMart() {
     return (
-        <div className="flex flex-wrap">
-            {Array(100).fill("").map((item, index) => (
-                <div key={index} className="p-10 ">Instamart</div>
-            ))}
+        <div className="flex flex-col flex-wrap gap-10 p-10 mb-10">
+            <InstaMartAccordion title={"Team"} />
+            <InstaMartAccordion title={"Store"} />
+            <InstaMartAccordion title={"Services"} />
         </div>
     )
 }
 export default InstaMart;
+
+
+
