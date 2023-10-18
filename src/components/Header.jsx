575389d044e5410d52eba6b72549fbcc0cae8f2c
Sachin Arya <sachin.arya@preplaced.in>
2023-10-18 22:06:10 +0530

    subscribe to the store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,15 @@ import { logo } from "../constants/constants"
 import { Link } from "react-router-dom";
 import useOnline from "../hooks/useOnline";
 import UserContext from "../context/UserContext";
+import { useSelector } from "react-redux";
 
 function Header() {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
     const isOnline = useOnline();
     const { user, setUser } = useContext(UserContext);
+
+    const cartItems = useSelector(store => store.cart.items);
+    console.log("cartItems", cartItems);
 
     return (
         <div className="flex justify-between items-center p-2 bg-blue-100 shadow-md">
@@ -19,7 +23,7 @@ function Header() {
                 <Link to="/about"> <li>About</li> </Link>
                 <Link to="/contact"><li>Contact</li></Link>
                 <Link to="/instamart"><li>InstaMart</li></Link>
-                <Link to="/cart"><li>Cart</li></Link>
+                <Link to="/cart"><li>Cart [{cartItems.length}]</li></Link>
             </ul>
             <div className="flex">
                 <button className="px-5 py-2 font-medium" onClick={() => setIsLoggedIn(!isLoggedIn)}>{isLoggedIn ? "Logout" : "Login"}</button>
