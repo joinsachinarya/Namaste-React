f67049c73c0bcc6daf9cf37f0a1692d9aaf907f6
Sachin Arya <sachin.arya@preplaced.in>
2023-10-18 21:54:17 +0530

    create cartSlice and configure store

--- App.js
+++ App.js
@@ -3,18 +3,23 @@ import Footer from "./src/components/Footer";
 import { Outlet } from "react-router-dom";
 import UserContext from "./src/context/UserContext";
 import { useState } from "react";
+import { Provider } from "react-redux";
+import store from "./src/store/store";
 
 function App() {
     const [user, setUser] = useState({
         name: "Sachin Arya",
         email: "sachinkarya333@gmail.com"
     })
+    console.log("store", store);
     return (
-        <UserContext.Provider value={{ user: user, setUser: setUser }}>
-            <Header />
-            <Outlet />
-            <Footer />
-        </UserContext.Provider>
+        <Provider store={store}>
+            <UserContext.Provider value={{ user: user, setUser: setUser }}>
+                <Header />
+                <Outlet />
+                <Footer />
+            </UserContext.Provider>
+        </Provider>
     )
 }
 
---
+++ src/store/cartSlice.js
@@ -0,0 +1,22 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: ["dummy", "item"]
+    },
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items.pop();
+        },
+        clearCart: (state, action) => {
+            state.items = [];
+        }
+    }
+})
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
